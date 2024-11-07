5a6677a359996757f7393f6d65f9696c5bd3db6c
WithBaz <jorgesebastianflorezdiaz2004@gmail.com>
2024-11-07 17:49:58 -0500

    Base parcial

---
+++ src/test/java/co/edu/ufps/Segundo_Previo/SegundoPrevioApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.ufps.Segundo_Previo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SegundoPrevioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
